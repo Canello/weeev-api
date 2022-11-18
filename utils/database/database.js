2adf745896061167942d0add6c1d8ebc61f2ee01
Gustavo Canello <gustavobrandicanello@gmail.com>
2022-11-18 17:30:52 -0300

    Arrumando conexão do postgresql para o Heroku.

--- utils/database/database.js
+++ utils/database/database.js
@@ -1,11 +1,19 @@
+const CONNECTION = process.env.MODE === 'dev' ?
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }
+    :
+    {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    };
+
 const db = require('knex')({
     client: 'pg',
-    connection: {
-        host : process.env.DB_HOST,
-        user : process.env.DB_USER,
-        password : process.env.DB_PASSWORD,
-        database : process.env.DB_NAME
-    }
+    connection: CONNECTION
 });
 
 module.exports = db;
